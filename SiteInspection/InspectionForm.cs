7f31e8dcb6492bb8a916ff8be0a523d1ee03a242
ethandsz <ethanzizu10@gmail.com>
2021-10-20 08:31:49 -0400

    Removed code from the old database structure

--- SiteInspection/InspectionForm.cs
+++ SiteInspection/InspectionForm.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient;
 
 namespace SiteInspection
 {
@@ -16,18 +15,18 @@ namespace SiteInspection
     {
         FillingForm fil_form = new FillingForm();
         //Path to connect to the database
-        string path = @"Data Source=DESKTOP-0ONOATC\SQLEXPRESS;Initial Catalog=SiteInspection;Integrated Security=True";//Change data source = (Your data source from your computer) and Initial Catalog = (Your created database, unless you used the same name)
+        //string path = @"Data Source=DESKTOP-0ONOATC\SQLEXPRESS;Initial Catalog=SiteInspection;Integrated Security=True";//Change data source = (Your data source from your computer) and Initial Catalog = (Your created database, unless you used the same name)
         
         //Creating the connection and command objs
-        SqlConnection con;
-        SqlCommand cmd;
+        //SqlConnection con;
+        //SqlCommand cmd;
        
         //Variables
         string date;
         public InspectionForm()
         {
             InitializeComponent();
-            con = new SqlConnection(path); //Setting the connecting object and passing the path to it
+            //con = new SqlConnection(path); //Setting the connecting object and passing the path to it
         }
 
         private void InspectionForm_Load(object sender, EventArgs e)
@@ -45,10 +44,10 @@ namespace SiteInspection
             {
                 try
                 {
-                    con.Open(); //Opening the connection to the database
-                    cmd = new SqlCommand("insert into Information (Site_Location,Date_Created,Completed_By,Supervisor,Inspector,Job_Description,Type_of_Job,Work_Area) values('" + txtSite.Text + "','" + date + "','" + txtCmpltd.Text + "','" + txtSupr.Text + "','" + txtInspc.Text + "','" + txtDesc.Text + "','" + txtType.Text + "','" + txtWrk_Area.Text + "')", con); //Saves the data from the textbox to the command object and gets it ready to send to the database. (Change 'Information' to your created table in the database unless you also used 'Information')
-                    cmd.ExecuteNonQuery(); //Executes the exchange of information from the form to the database
-                    con.Close(); //Closes the connection to the database
+                    //con.Open(); //Opening the connection to the database
+                    //cmd = new SqlCommand("insert into Information (Site_Location,Date_Created,Completed_By,Supervisor,Inspector,Job_Description,Type_of_Job,Work_Area) values('" + txtSite.Text + "','" + date + "','" + txtCmpltd.Text + "','" + txtSupr.Text + "','" + txtInspc.Text + "','" + txtDesc.Text + "','" + txtType.Text + "','" + txtWrk_Area.Text + "')", con); //Saves the data from the textbox to the command object and gets it ready to send to the database. (Change 'Information' to your created table in the database unless you also used 'Information')
+                    //cmd.ExecuteNonQuery(); //Executes the exchange of information from the form to the database
+                    //con.Close(); //Closes the connection to the database
                     MessageBox.Show("Your data has been saved into the database");
                     clear();
                     fil_form.Show();
