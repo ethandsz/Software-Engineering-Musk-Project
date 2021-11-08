ff12ba0f4efed2e8d49a941fce5bfbd284668b2c
Ben Nemeth <BN311@student.aru.ac.uk>
2021-11-08 01:09:45 +0000

    Tiny tweaks

--- SiteInspection/Form2.Designer.cs
+++ SiteInspection/Form2.Designer.cs
@@ -30,8 +30,8 @@ namespace SiteInspection
         private void InitializeComponent()
         {
             this.UserPassField = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.enterBtn = new System.Windows.Forms.Button();
+            this.backBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // UserPassField
@@ -41,25 +41,25 @@ namespace SiteInspection
             this.UserPassField.Size = new System.Drawing.Size(100, 20);
             this.UserPassField.TabIndex = 0;
             // 
-            // button1
+            // enterBtn
             // 
-            this.button1.Location = new System.Drawing.Point(100, 54);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Enter";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.enterBtn.Location = new System.Drawing.Point(100, 54);
+            this.enterBtn.Name = "enterBtn";
+            this.enterBtn.Size = new System.Drawing.Size(75, 23);
+            this.enterBtn.TabIndex = 1;
+            this.enterBtn.Text = "Enter";
+            this.enterBtn.UseVisualStyleBackColor = true;
+            this.enterBtn.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // backBtn
             // 
-            this.button2.Location = new System.Drawing.Point(100, 76);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Back";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.backBtn.Location = new System.Drawing.Point(100, 76);
+            this.backBtn.Name = "backBtn";
+            this.backBtn.Size = new System.Drawing.Size(75, 23);
+            this.backBtn.TabIndex = 2;
+            this.backBtn.Text = "Back";
+            this.backBtn.UseVisualStyleBackColor = true;
+            this.backBtn.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form2
             // 
@@ -67,11 +67,12 @@ namespace SiteInspection
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(284, 111);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.backBtn);
+            this.Controls.Add(this.enterBtn);
             this.Controls.Add(this.UserPassField);
             this.Name = "Form2";
-            this.Text = "Admin Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Enter Admin Password";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,7 +81,7 @@ namespace SiteInspection
         #endregion
 
         private System.Windows.Forms.TextBox UserPassField;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button enterBtn;
+        private System.Windows.Forms.Button backBtn;
     }
 }
